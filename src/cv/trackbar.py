3c814e7d1daec75bb2cac5945a0104b0b0fd4891
nkilders <noah.kilders@gmail.com>
2022-11-07 15:37:17 +0100

    Faktor 2 oder 3

--- src/cv/trackbar.py
+++ src/cv/trackbar.py
@@ -3,16 +3,10 @@ from main import show_trackbars
 
 
 def create_window(window_name: str) -> None:
-    if not show_trackbars():
-        return
-        
     cv2.namedWindow(window_name)
 
 
 def create_trackbar(window_name: str, trackbar_name: str, val_name: str, max_value: int) -> None:
-    if not show_trackbars():
-        return
-
     cv2.createTrackbar(trackbar_name, window_name, VALUES.__getattribute__(val_name), max_value, lambda new_value: update_value(val_name, new_value))
 
 
@@ -49,22 +43,23 @@ VALUES = Values()
 
 
 
-create_window(NW_BLOCKS)
-create_trackbar(NW_BLOCKS, 'Blur Kernel',       'B_BLUR_KERNEL',        10)
-create_trackbar(NW_BLOCKS, 'Mask Lower H',      'B_MASK_LOWER_H',       255)
-create_trackbar(NW_BLOCKS, 'Mask Lower S',      'B_MASK_LOWER_S',       255)
-create_trackbar(NW_BLOCKS, 'Mask Lower V',      'B_MASK_LOWER_V',       255)
-create_trackbar(NW_BLOCKS, 'Mask Upper H',      'B_MASK_UPPER_H',       255)
-create_trackbar(NW_BLOCKS, 'Mask Upper S',      'B_MASK_UPPER_S',       255)
-create_trackbar(NW_BLOCKS, 'Mask Upper V',      'B_MASK_UPPER_V',       255)
-create_trackbar(NW_BLOCKS, 'Canny 1',           'B_CANNY_1',            255)
-create_trackbar(NW_BLOCKS, 'Canny 2',           'B_CANNY_2',            255)
-create_trackbar(NW_BLOCKS, 'Dilate Kernel',     'B_DILATE_K_SIZE',      10)
-create_trackbar(NW_BLOCKS, 'Min Contour Area',  'B_MIN_CONTOUR_AREA',   500)
-create_trackbar(NW_BLOCKS, 'Corner Accuracy',   'B_CORNER_ACCURACY',    1000)
+if show_trackbars():
+    create_window(NW_BLOCKS)
+    create_trackbar(NW_BLOCKS, 'Blur Kernel',       'B_BLUR_KERNEL',        10)
+    create_trackbar(NW_BLOCKS, 'Mask Lower H',      'B_MASK_LOWER_H',       255)
+    create_trackbar(NW_BLOCKS, 'Mask Lower S',      'B_MASK_LOWER_S',       255)
+    create_trackbar(NW_BLOCKS, 'Mask Lower V',      'B_MASK_LOWER_V',       255)
+    create_trackbar(NW_BLOCKS, 'Mask Upper H',      'B_MASK_UPPER_H',       255)
+    create_trackbar(NW_BLOCKS, 'Mask Upper S',      'B_MASK_UPPER_S',       255)
+    create_trackbar(NW_BLOCKS, 'Mask Upper V',      'B_MASK_UPPER_V',       255)
+    create_trackbar(NW_BLOCKS, 'Canny 1',           'B_CANNY_1',            255)
+    create_trackbar(NW_BLOCKS, 'Canny 2',           'B_CANNY_2',            255)
+    create_trackbar(NW_BLOCKS, 'Dilate Kernel',     'B_DILATE_K_SIZE',      10)
+    create_trackbar(NW_BLOCKS, 'Min Contour Area',  'B_MIN_CONTOUR_AREA',   500)
+    create_trackbar(NW_BLOCKS, 'Corner Accuracy',   'B_CORNER_ACCURACY',    1000)
 
-create_window(NW_SHADOW)
-create_trackbar(NW_SHADOW, 'Canny 1',           'S_CANNY_1',            1000)
-create_trackbar(NW_SHADOW, 'Canny 2',           'S_CANNY_2',            1000)
-create_trackbar(NW_SHADOW, 'Min Contour Area',  'S_MIN_CONTOUR_AREA',   500)
-create_trackbar(NW_SHADOW, 'Corner Accuracy',   'S_CORNER_ACCURACY',    100)
+    create_window(NW_SHADOW)
+    create_trackbar(NW_SHADOW, 'Canny 1',           'S_CANNY_1',            1000)
+    create_trackbar(NW_SHADOW, 'Canny 2',           'S_CANNY_2',            1000)
+    create_trackbar(NW_SHADOW, 'Min Contour Area',  'S_MIN_CONTOUR_AREA',   500)
+    create_trackbar(NW_SHADOW, 'Corner Accuracy',   'S_CORNER_ACCURACY',    100)
