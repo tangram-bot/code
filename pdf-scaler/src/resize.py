9e108c2b890e56329528b967cbf57596b333f483
akkjon <akkjon@outlook.de>
2022-11-17 21:27:19 +0100

    add pdf-resize tool

---
+++ pdf-scaler/src/resize.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+import os
+
+path = "cyk-cyk/"
+files = os.listdir(path)
+fac = 0.955
+
+dimensions = (3508, 2480)
+scaled_dimensions = (int(dimensions[0] * fac), int(dimensions[1] * fac))
+
+width, height = scaled_dimensions
+x, y = int((dimensions[0] - scaled_dimensions[0]) / 2), int((dimensions[1] - scaled_dimensions[1]) / 2)
+
+for file in files:
+    print(file)
+    img = cv2.imread(path + file, 1)
+
+    # Crop image to specified area using slicing
+    crop_img = img[y:y+height, x:x+width]
+    cv2.imwrite(path + file + "-new.png", crop_img)
